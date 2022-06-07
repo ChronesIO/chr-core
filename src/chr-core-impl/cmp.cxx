2c65a8ea2754f3ab4d4d99deacb291a0e96deaab
CXCubeHD <cxcubehd@gmail.com>
2022-06-07 10:50:05 +0200

    Updated .gitignore; Added std-func.cc; and more...

--- src/chr-core-impl/cmp.cxx
+++ src/chr-core-impl/cmp.cxx
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <memory>
 
+#include "chr-core-impl/std/std-fun.cc"
 #include "inc"
 
 void test()
@@ -7,4 +9,9 @@ void test()
     using namespace chr;
 
     str s = "Hello World";
+
+    str s2 = "Hello World 2";
+
+    sp<int> a = sp_new<int>(10);
+
 }
