82472d257ba1514e846fbcf46b8e0ca75e04ceb2
CXCubeHD <cxcubehd@gmail.com>
2022-06-12 21:57:15 +0200

    removed chr::typeh(chr::any), chr::typen(chr::any)

--- src/chr-core-impl/std/std-typ.cc
+++ src/chr-core-impl/std/std-typ.cc
@@ -14,12 +14,6 @@ namespace chr
     template <class T> auto TypeHash() -> Hash { return typeid(T).hash_code(); }
     template <class T> auto typeh() -> hash { return typeid(T).hash_code(); }
 
-    inline auto TypeHash(const Any& a) -> Hash { return a.type().hash_code(); }
-    inline auto typeh(const any& a) -> hash { return a.type().hash_code(); }
-
     template <class T> auto TypeName() -> String { return typeid(T).name(); }
     template <class T> auto typen() -> str { return typeid(T).name(); }
-
-    inline auto TypeName(const Any& a) -> String { return a.type().name(); }
-    inline auto typen(const any& a) -> str { return a.type().name(); }
 }
