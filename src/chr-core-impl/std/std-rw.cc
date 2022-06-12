27cf9a55eb62e031305760f427fabf1828b38a5f
CXCubeHD <cxcubehd@gmail.com>
2022-06-12 20:05:54 +0200

    Added chr::ref(...)

--- src/chr-core-impl/std/std-rw.cc
+++ src/chr-core-impl/std/std-rw.cc
@@ -6,4 +6,20 @@ namespace chr
 {
     template <class T> using RefWrapper = std::reference_wrapper<T>;
     template <class T> using rw = RefWrapper<T>;
+
+    template <class T> constexpr auto Ref(T& v) noexcept -> RefWrapper<T> { return std::ref(v); }
+    template <class T> constexpr auto Ref(RefWrapper<T> v) noexcept -> RefWrapper<T> { return std::ref(v); }
+    template <class T> void Ref(const T&&) = delete;
+
+    template <class T> constexpr auto CRef(const T& v) noexcept -> RefWrapper<const T> { return std::cref(v); }
+    template <class T> constexpr auto CRef(RefWrapper<T> v) noexcept -> RefWrapper<const T>;
+    template <class T> void CRef(const T&&) = delete;
+
+    template <class T> constexpr auto ref(T& v) noexcept -> rw<T> { return std::ref(v); }
+    template <class T> constexpr auto ref(rw<T> v) noexcept -> rw<T> { return std::ref(v); }
+    template <class T> void ref(const T&&) = delete;
+
+    template <class T> constexpr auto cref(const T& v) noexcept -> rw<const T> { return std::cref(v); }
+    template <class T> constexpr auto cref(rw<T> v) noexcept -> rw<const T>;
+    template <class T> void cref(const T&&) = delete;
 }
