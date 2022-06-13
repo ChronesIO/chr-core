e12d18b89154585cc48c8f1b728bc65123c51d27
CXCubeHD <cxcubehd@gmail.com>
2022-06-13 20:10:27 +0200

    fixed unimplemented functions

--- src/chr-core-impl/std/std-rw.cc
+++ src/chr-core-impl/std/std-rw.cc
@@ -12,7 +12,7 @@ namespace chr
     template <class T> void Ref(const T&&) = delete;
 
     template <class T> constexpr auto CRef(const T& v) noexcept -> RefWrapper<const T> { return std::cref(v); }
-    template <class T> constexpr auto CRef(RefWrapper<T> v) noexcept -> RefWrapper<const T>;
+    template <class T> constexpr auto CRef(RefWrapper<T> v) noexcept -> RefWrapper<const T> { return std::cref(v); }
     template <class T> void CRef(const T&&) = delete;
 
     template <class T> constexpr auto ref(T& v) noexcept -> rw<T> { return std::ref(v); }
@@ -20,6 +20,6 @@ namespace chr
     template <class T> void ref(const T&&) = delete;
 
     template <class T> constexpr auto cref(const T& v) noexcept -> rw<const T> { return std::cref(v); }
-    template <class T> constexpr auto cref(rw<T> v) noexcept -> rw<const T>;
+    template <class T> constexpr auto cref(rw<T> v) noexcept -> rw<const T> { return std::cref(v); }
     template <class T> void cref(const T&&) = delete;
 }
