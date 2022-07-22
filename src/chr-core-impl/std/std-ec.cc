93385c30be9b3ccffc8e2be1e198da464ca00a03
CXCubeHD <cxcubehd@gmail.com>
2022-07-22 17:34:49 +0200

    added chr::ec

---
+++ src/chr-core-impl/std/std-ec.cc
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <utility>
+
+namespace chr
+{
+    template <class T, class U = T>
+    constexpr T ec(T& obj, U&& new_value) noexcept(
+        std::is_nothrow_move_constructible_v<T>&& std::is_nothrow_assignable_v<T&, U>)
+    {
+        return std::exchange(obj, new_value);
+    }
+}
